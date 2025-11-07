5b2aaa9b8a0a134d49135ceb68caf1658e2c2ae9
Harsh Ranjan <142663690+harshranjan0522@users.noreply.github.com>
2025-11-07 12:14:53 +0530

    Create AccountDAO.java

---
+++ EXPERIMENT 8/c/AccountDAO.java
@@ -0,0 +1,20 @@
+package com.example.transactiondemo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AccountDAO {
+    @Autowired
+    private SessionFactory factory;
+
+    public Account getAccount(int id) {
+        return factory.getCurrentSession().get(Account.class, id);
+    }
+
+    public void updateAccount(Account acc) {
+        factory.getCurrentSession().update(acc);
+    }
+}
