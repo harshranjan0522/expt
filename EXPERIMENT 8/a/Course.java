53baa69f3e7426e904ea1f9cd4c80d11453c6ac7
Harsh Ranjan <142663690+harshranjan0522@users.noreply.github.com>
2025-11-07 12:10:26 +0530

    Create Course.java

---
+++ EXPERIMENT 8/a/Course.java
@@ -0,0 +1,13 @@
+package com.example.springdemo;
+
+public class Course {
+    private String courseName;
+
+    public Course() {
+        this.courseName = "Java Programming";
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+}
