59a5c13bfa39b709b008b69fb1584b19b7e9b762
Harsh Ranjan <142663690+harshranjan0522@users.noreply.github.com>
2025-11-07 12:05:07 +0530

    Create Student.java

---
+++ EXPERIMENT 6/c/Student.java
@@ -0,0 +1,26 @@
+package c;
+public class Student {
+    private int studentID;
+    private String name;
+    private String department;
+    private double marks;
+
+    // Constructor
+    public Student(int studentID, String name, String department, double marks) {
+        this.studentID = studentID;
+        this.name = name;
+        this.department = department;
+        this.marks = marks;
+    }
+
+    // Getters
+    public int getStudentID() { return studentID; }
+    public String getName() { return name; }
+    public String getDepartment() { return department; }
+    public double getMarks() { return marks; }
+
+    // Setters
+    public void setName(String name) { this.name = name; }
+    public void setDepartment(String department) { this.department = department; }
+    public void setMarks(double marks) { this.marks = marks; }
+}
