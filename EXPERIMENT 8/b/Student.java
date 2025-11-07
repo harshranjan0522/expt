6cfff6f237f9b0526f93d1b9c42f25abcf02fc6e
Harsh Ranjan <142663690+harshranjan0522@users.noreply.github.com>
2025-11-07 12:13:39 +0530

    Create Student.java

---
+++ EXPERIMENT 8/b/Student.java
@@ -0,0 +1,27 @@
+package com.example.hibernatecrud;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String course;
+
+    public Student() {}
+    public Student(String name, String course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    // Getters & Setters
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getCourse() { return course; }
+    public void setName(String name) { this.name = name; }
+    public void setCourse(String course) { this.course = course; }
+}
