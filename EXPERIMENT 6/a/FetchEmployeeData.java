a6f2f2e56f8dbff1cc969e2c451066ba7b3137c5
Harsh Ranjan <142663690+harshranjan0522@users.noreply.github.com>
2025-12-29 15:37:28 +0530

    Update FetchEmployeeData.java

--- EXPERIMENT 6/a/FetchEmployeeData.java
+++ EXPERIMENT 6/a/FetchEmployeeData.java
@@ -2,7 +2,7 @@ package a;
 import java.sql.*;
 
 public class FetchEmployeeData {
-
+//main file to fetch employee data
     public static void main(String[] args) {
         // Database credentials
         String url = "jdbc:mysql://localhost:3306/companydb"; // replace 'companydb' with your DB name
